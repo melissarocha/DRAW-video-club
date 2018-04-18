0254b7b3cf353879e796eac6a0caf38810420f1f
Melissa Rocha <emelissaro97@gmail.com>
2018-04-18 09:30:54 -0600

    Se agrega update al controlador de movies. Se agrega el archivo README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proyecto de Video videoclub
+
+Este es el proyecto de video club para la clase de DRAW.
+
+## Como instalar
+```
+git clone git@github.com:melissarocha/DRAW-video-club.git
+
+```
+
+```
+cd VideoClub\
+```
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,7 +10,7 @@ function index(request, response, next){
   });
 }
 
-function create(request, response, next){
+function create(req, response, next){
   const title = req.body.title;
   const genre = req.body.genre;
   const duration = req.body.duration;
@@ -22,29 +22,66 @@ function create(request, response, next){
   movie.duration = duration;
   movie.director = director;
 
-  movie.save((err, obj)=>);
-  if(err){
-    response.json({
-      error: false,
-      message: 'Pelicula Guardada',
-      obj: obj
-    })
-  }else{
-    response.json({
-      error: false,
-      message: 'Pelicula Guardada',
-      obj: obj
-    })
-  }
-  //response.send("Estas en /movies/ -> POST");
+  movie.save((err, obj)=>{
+    if(err){
+      response.json({
+        error: false,
+        message: 'Pelicula Guardada',
+        obj: obj
+      })
+    }else{
+      response.json({
+        error: false,
+        message: 'Pelicula Guardada',
+        obj: obj
+      })
+    }
+    //response.send("Estas en /movies/ -> POST");
+  });
 }
 
 function update(request, response, next){
-  response.send("Estas en /movies/ -> PUT");
+  const id = request.params.id;
+  Movie.update({_id:id}, {$set: request.body})
+    .exec()
+    .then(result => {
+      console.log(result);
+      response.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      response.status(500).json({
+        error: err
+      });
+    });
 }
 
+
 function remove(request, response, next){
-  response.send("Estas en /movies/ -> DELETE");
+  const id = request.params.id;
+  if(id){
+    Movie.remove({_id:id}, function(err){
+      if(err){
+        response.json({
+          error: true,
+          message: 'Pelicula No Eliminada',
+          obj: {}
+        })
+      }else{
+        response.json({
+          error: false,
+          message: 'Pelicula Eliminada',
+          obj: {}
+        })
+      }
+    })
+  }else{
+    response.json({
+      error: true,
+      message: 'Pelicula No Existe',
+      obj: {}
+    })
+  }
 }
 
 module.exports = {
